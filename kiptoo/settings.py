c323ca69008b80059179575331b923500dfd272d
datakiptoo <105823532+datakiptoo@users.noreply.github.com>
2023-02-10 05:13:57 +0300

    modified:   kiptoo/__pycache__/settings.cpython-39.pyc 	modified:   kiptoo/settings.py     modified:   members/__pycache__/urls.cpython-39.pyc    modified:   members/__pycache__/views.cpython-39.pyc 	new file:   members/templates/404.html 	new file:   members/templates/allmembers.html 	new file:   members/templates/details.html 	new file:   members/templates/main.html 	new file:   members/templates/master.html 	new file:   members/templates/template.html 	modified:   members/urls.py 	modified:   members/views.py

--- kiptoo/settings.py
+++ kiptoo/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+  #SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
--- members/views.py
+++ members/views.py
@@ -1,8 +1,34 @@
 
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 # Create your views here.
 
 def members(request):
-    template= loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers=Member.objects.all().values()
+    #Creates a mymembers object with all the values of the Member model
+    template= loader.get_template('allmembers.html')#Loads the all_members.html template.
+    context={'mymembers':mymembers,
+             }#Creates an object containing the mymembers object
+    return HttpResponse(template.render(context,request))#Sends the object to the template.
+#Outputs the HTML that is rendered by the template
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)#Gets the id as an argument
+  #Uses the id to locate the correct record in the Member table.
+  template = loader.get_template('details.html')#loads the details.html template.
+  context = {
+    'mymember': mymember,
+  }#Creates an object containing the member.
+  return HttpResponse(template.render(context, request))#Sends the object to the template.
+#Outputs the HTML that is rendered by the template
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
